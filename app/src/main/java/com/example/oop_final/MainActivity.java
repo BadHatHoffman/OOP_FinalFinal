c002307843819fefd0b0fb7e6d4851ced9b4ef37
BadHatHoffman <EHoffman@student.neumont.edu>
2020-03-03 15:12:12 -0700

    merge

--- app/src/main/java/com/example/oop_final/MainActivity.java
+++ app/src/main/java/com/example/oop_final/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.oop_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import android.app.Activity;
-=======
 import android.content.Intent;
->>>>>>> c1e5ec6b9709f31f387bf8e34f71fa45a75ee593
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -22,15 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-<<<<<<< HEAD
     public void openingScreen() {
 
-=======
+    }
     public void beginClick(View v){
         Intent intent = new Intent(this, CharacterMenu.class);
         intent.putExtras(bundle);
         startActivity(intent);
->>>>>>> c1e5ec6b9709f31f387bf8e34f71fa45a75ee593
     }
 
 
