b920ba0b74ec1e2399a10469c141dd45c9cda7ab
KaylaMcAuliff <KaylaMcAuliff@users.github.com>
2020-03-04 20:11:44 -0700

    more styling as well as base layout

--- app/src/main/java/com/example/oop_final/CharacterMenu.java
+++ app/src/main/java/com/example/oop_final/CharacterMenu.java
@@ -3,11 +3,13 @@ package com.example.oop_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class CharacterMenu extends AppCompatActivity {
     public Bundle bundles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_menu);
         bundles = getIntent().getExtras();
--- app/src/main/java/com/example/oop_final/DungeonMenu.java
+++ app/src/main/java/com/example/oop_final/DungeonMenu.java
@@ -3,11 +3,13 @@ package com.example.oop_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class DungeonMenu extends AppCompatActivity {
     public Bundle bundles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dungeon_menu);
         bundles = getIntent().getExtras();
